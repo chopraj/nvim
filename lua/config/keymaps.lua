414c89ea5c0867cc01e1d47db48d644d0b9cf307
Raj Chopra <raj.chopra.az@gmail.com>
2025-09-02 18:59:53 -0700

    init

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,34 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Cursor-like keybindings
+-- Cmd+F for local file search (current buffer search)
+vim.keymap.set("n", "<D-f>", "/", { desc = "Search in current buffer" })
+
+-- Standard editor shortcuts
+-- Cmd+Z for undo (works in normal and insert mode)
+vim.keymap.set("n", "<D-z>", "<cmd>undo<cr>", { desc = "Undo" })
+vim.keymap.set("i", "<D-z>", "<C-o><cmd>undo<cr>", { desc = "Undo" })
+
+-- Cmd+S for save (works in normal and insert mode)
+vim.keymap.set("n", "<D-s>", "<cmd>w<cr>", { desc = "Save file" })
+vim.keymap.set("i", "<D-s>", "<C-o><cmd>w<cr>", { desc = "Save file" })
+
+-- Cmd+A for select all (works in normal and insert mode)
+vim.keymap.set("n", "<D-a>", "ggVG", { desc = "Select all" })
+vim.keymap.set("i", "<D-a>", "<C-o>ggVG", { desc = "Select all" })
+
+-- Clipboard shortcuts (works in normal, visual, and insert mode)
+-- Cmd+C for copy
+vim.keymap.set("v", "<D-c>", '"+y', { desc = "Copy to clipboard" })
+vim.keymap.set("n", "<D-c>", '"+yy', { desc = "Copy line to clipboard" })
+
+-- Cmd+V for paste
+vim.keymap.set("n", "<D-v>", '"+p', { desc = "Paste from clipboard" })
+vim.keymap.set("v", "<D-v>", '"+p', { desc = "Paste from clipboard" })
+vim.keymap.set("i", "<D-v>", '<C-r>+', { desc = "Paste from clipboard" })
+
+-- Cmd+X for cut
+vim.keymap.set("v", "<D-x>", '"+x', { desc = "Cut to clipboard" })
+vim.keymap.set("n", "<D-x>", '"+dd', { desc = "Cut line to clipboard" })
---
+++ lua/plugins/example.lua
@@ -0,0 +1,95 @@
+return {
+  -- Add kanagawa colorscheme
+  { "rebelot/kanagawa.nvim" },
+
+  -- Configure LazyVim to load kanagawa
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+
+  -- Better icons with material design
+  {
+    "nvim-tree/nvim-web-devicons",
+    opts = {
+      override = {
+        zsh = {
+          icon = "",
+          color = "#428850",
+          cterm_color = "65",
+          name = "Zsh"
+        }
+      },
+      color_icons = true,
+      default = true,
+      strict = true,
+      override_by_filename = {
+        [".gitignore"] = {
+          icon = "",
+          color = "#f1502f",
+          name = "Gitignore"
+        }
+      },
+      override_by_extension = {
+        ["log"] = {
+          icon = "",
+          color = "#81e043",
+          name = "Log"
+        }
+      },
+    }
+  },
+
+  -- Configure Telescope with Cursor-like keybindings
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- Cmd+P for file name search (works in normal and insert mode)
+      { "<D-p>", "<cmd>Telescope find_files<cr>", desc = "Find files", mode = { "n", "i" } },
+      -- Cmd+Shift+F for global text search (works in normal and insert mode)
+      { "<D-S-f>", "<cmd>Telescope live_grep<cr>", desc = "Search in all files", mode = { "n", "i" } },
+    },
+  },
+
+  -- Configure Neo-tree with Cursor-like keybindings and show hidden files
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      -- Cmd+B for file tree toggle (works in normal and insert mode)
+      { "<D-b>", "<cmd>Neotree toggle<cr>", desc = "Toggle file tree", mode = { "n", "i" } },
+    },
+    opts = {
+      filesystem = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_hidden = false,
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false,
+          show_hidden_count = false,
+        },
+      },
+      default_component_configs = {
+        icon = {
+          provider = function(icon, node, state)
+            if node.type == "file" or node.type == "terminal" then
+              local success, web_devicons = pcall(require, "nvim-web-devicons")
+              local name = node.type == "terminal" and "terminal" or node.name
+              if success then
+                local devicon, hl = web_devicons.get_icon(name)
+                icon.text = devicon or icon.text
+                icon.highlight = hl or icon.highlight
+              end
+            end
+          end,
+        },
+      },
+    },
+  },
+
+}
